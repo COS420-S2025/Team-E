d555ae954162b25ca1b76a94e4b3487206a88945
GraceC-326 <grace.a.cochran@maine.edu>
2026-02-24 19:58:04 -0500

    Updated App.css/.tsx (created header & place to put names)

--- Code/spec_effect/src/App.tsx
+++ Code/spec_effect/src/App.tsx
@@ -4,6 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <header className="App-header"> 
+          SpecEffect 
+      </header>
+      <header className="App-subHeader"> 
+          glossary/about buttons and search bar here maybe
+      </header>
+      <p>
+            Grace Cochran, [Insert rest of names here]
+      </p>
     </div>
   );
 }
