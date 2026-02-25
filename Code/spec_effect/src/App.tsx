1c0c2cad8d555e5f9f56ae326faa414e311895ef
Aragon9723 <160268275+Aragon9723@users.noreply.github.com>
2026-02-24 22:25:54 -0500

    Update App.tsx
    Added name for myself. Also reformatted the file.

--- Code/spec_effect/src/App.tsx
+++ Code/spec_effect/src/App.tsx
@@ -1,20 +1,18 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header"> 
-          SpecEffect 
-      </header>
-      <header className="App-subHeader"> 
-          glossary/about buttons and search bar here maybe
-      </header>
-      <p>
-            Grace Cochran, Steven Harrington, Eliot Cole, [Insert rest of names here]
-      </p>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">SpecEffect</header>
+            <header className="App-subHeader">
+                glossary/about buttons and search bar here maybe
+            </header>
+            <p>
+                Grace Cochran, Steven Harrington, Eliot Cole, Caramon Cotroneo
+            </p>
+        </div>
+    );
 }
 
 export default App;
