93d6796dcbad7924447df7c31ccd2faa00a0d73b
GraceC-326 <grace.a.cochran@maine.edu>
2026-03-03 20:06:39 -0500

    Fixed style of About and Glossary buttons

--- Code/spec_effect/src/App.tsx
+++ Code/spec_effect/src/App.tsx
@@ -9,7 +9,8 @@ function App() {
                 <img src = {seLogo} alt="" style={{ width: '50px', height: 'auto', marginRight: '10px' }}/> SpecEffect
             </header>
             <header className="App-subHeader">
-                glossary/about buttons and search bar here maybe
+                <button className="App-headerButtonStyle">About</button> 
+                <button className="App-headerButtonStyle">Glossary</button>
             </header>
             <p>
                 Grace Cochran, Steven Harrington, Eliot Cole, Caramon Cotroneo
