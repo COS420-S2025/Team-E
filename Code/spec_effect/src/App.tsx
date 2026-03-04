a6b8b5be664a21b0b3cc32de36b4a9feeb56d1f9
GraceC-326 <grace.a.cochran@maine.edu>
2026-03-03 19:27:37 -0500

    Added an image file and a logo

--- Code/spec_effect/src/App.tsx
+++ Code/spec_effect/src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
+import seLogo from "./Images/SpecEffect_Logo.png"
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">SpecEffect</header>
+            <header className="App-header">
+                <img src = {seLogo} alt="" style={{ width: '50px', height: 'auto', marginRight: '10px' }}/> SpecEffect
+            </header>
             <header className="App-subHeader">
                 glossary/about buttons and search bar here maybe
             </header>
