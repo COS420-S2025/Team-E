1287949ac5036a52c188f7930db1065bbb9fedf9
GraceC-326 <grace.a.cochran@maine.edu>
2026-03-24 13:31:43 -0400

    Copied tests from App.test.tsx to Glossary.test.tsx

--- Code/spec_effect/src/pages/Glossary.test.tsx
+++ Code/spec_effect/src/pages/Glossary.test.tsx
@@ -1,5 +1,36 @@
 import React from "react";
-import "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import Glossary from "./Glossary";
 
+test("renders learn react link", () => {
+    render(
+        <MemoryRouter>
+            <Glossary />
+        </MemoryRouter>,
+    );
+    const linkElement = screen.getByText(/SpecEffect/i);
+    expect(linkElement).toBeInTheDocument();
+});
+
+describe("Some HTML Elements are added.", () => {
+    test("There is a heading", () => {
+        render(
+            <MemoryRouter>
+                <Glossary />
+            </MemoryRouter>,
+        );
+        const header = screen.getAllByRole("heading");
+        expect(header[0]).toBeInTheDocument();
+    });
+
+    test("There is an image", () => {
+        render(
+            <MemoryRouter>
+                <Glossary />
+            </MemoryRouter>,
+        );
+        const image = screen.getByRole("presentation");
+        expect(image).toBeInTheDocument();
+    });
+});
