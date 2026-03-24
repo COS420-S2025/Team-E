af149ebe3bddab3e87626bbfd6ab2a7cc7e3f9aa
GraceC-326 <grace.a.cochran@maine.edu>
2026-03-24 18:29:14 -0400

    Updated Glossary.tsx

--- Code/spec_effect/src/pages/Glossary.tsx
+++ Code/spec_effect/src/pages/Glossary.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 const Glossary = () => {
-    return <h2>Glossary Page</h2>;
+    return (<div>
+        <h2>Glossary Page</h2>
+                <div className="App-row">
+                    <div className="App-colBox" style={{width: "80vw", overflowY: "scroll",}}>
+                        put GlossaryText stuff in here  
+                    </div>
+                </div>
+        </div>
+    );
 };
 export default Glossary;
