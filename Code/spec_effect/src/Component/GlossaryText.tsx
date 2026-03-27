7dd7ed32c9921132d37ec0cba4b7338c65ab0442
GraceC-326 <grace.a.cochran@maine.edu>
2026-03-27 14:35:52 -0400

    Tweaked visuals of table of contents list.

--- Code/spec_effect/src/Component/GlossaryText.tsx
+++ Code/spec_effect/src/Component/GlossaryText.tsx
@@ -1,11 +1,5 @@
 import React from "react";
 
-/* NOTE:
-I'm expecting each item in the glossary database to have an ID number, a string for a term’s name, and a longer string defining the term. Im also assuming the glossary database is already in alphabetical order in terms of term name.
-
-The table of contents divider will containing a list of links that are labeled after each term name, and when clicked, scroll down to the definition of its term. The second divider after it will print the terms and definitions straight from the database, making the term names h3 and tying them to the links established in the table of contents divider.
-*/
-
 interface GlossItem {
     id: number;
     term: string;
@@ -32,8 +26,8 @@ const GlossaryText = () => {
     return (<div>
         <h3>Table of Contents</h3>
                 <div className="App-row">
-                    <div className="App-colBox" style={{width: "50vw"}}>
-                        <ul>
+                    <div className="App-colBox" style={{width: "50vw", textAlign: "left"}}>
+                        <ul style={{paddingLeft: "20vw"}}>
                         {mockGlossData.map((item) => (
                             <li key={item.id}>
                                 <a href={`#${createAnchor(item.term)}`}>
@@ -46,7 +40,7 @@ const GlossaryText = () => {
                 </div>
                 <div style={{ marginTop: "2rem" }}>
                 {mockGlossData.map((item) => (
-                    <div key={item.id} style={{ marginBottom: "1.5rem" }}>
+                    <div key={item.id}>
                         <h3 id={createAnchor(item.term)}>
                             {item.term}
                         </h3>
