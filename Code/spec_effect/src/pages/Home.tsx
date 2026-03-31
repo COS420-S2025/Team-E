7fedbadda8e2808921b73aa95ef6da7f9964e8a0
Aragon9723 <160268275+Aragon9723@users.noreply.github.com>
2026-03-31 10:48:37 -0400

    Added in tests to begin branch protection

--- Code/spec_effect/src/pages/Home.tsx
+++ Code/spec_effect/src/pages/Home.tsx
@@ -1,27 +1,30 @@
-import React from 'react';
+import React from "react";
 const Home = () => {
-    return (<div>
-        <h2>Home Page</h2>
+    return (
+        <div>
+            <h2>Home Page</h2>
             <div className="App-row">
-                <div className="App-colBox" style={{width: '150px', height: '370px'}}>
+                <div
+                    className="App-colBox"
+                    style={{ width: "150px", height: "370px" }}>
                     Soon to be Filter row divider
                 </div>
                 <div>
-                    <div className="App-colBox" style={{width: '900px', height: '20px'}}>
+                    <div
+                        className="App-colBox"
+                        data-testid="searchBarDiv"
+                        style={{ width: "900px", height: "20px" }}>
                         Soon to be Search Bar divider
                     </div>
-                    <div className="App-colBox" style={{width: '900px', height: '300px'}}>
+                    <div
+                        className="App-colBox"
+                        data-testid="catalogDiv"
+                        style={{ width: "900px", height: "300px" }}>
                         Soon to be Search results divider
                     </div>
                 </div>
             </div>
-    </div>
-        
+        </div>
     );
-    
 };
 export default Home;
-
-
-
-
