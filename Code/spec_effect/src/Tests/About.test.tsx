03952edc2976b93c863dce53635a6604328ff945
Aragon9723 <160268275+Aragon9723@users.noreply.github.com>
2026-04-03 14:47:11 -0400

    Moved tests and fixed import statements

--- Code/spec_effect/src/Tests/About.test.tsx
+++ Code/spec_effect/src/Tests/About.test.tsx
@@ -1,46 +1,45 @@
-import {render, screen} from '@testing-library/react';
+import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
-import {axe, toHaveNoViolations} from 'jest-axe';
-import About from './About';
+import { axe, toHaveNoViolations } from "jest-axe";
+import About from "../pages/About";
 
 describe("About page is rendered", () => {
-
-    
     //Make sure the main heading is present and correctly labeled.
-    test('renders the main heading', () => {
+    test("renders the main heading", () => {
         render(
             <MemoryRouter>
                 <About />
-            </MemoryRouter>
+            </MemoryRouter>,
         );
 
-        expect(screen.getByRole('heading', {name: /About Page/i })).toBeInTheDocument();
-    })
+        expect(
+            screen.getByRole("heading", { name: /About Page/i }),
+        ).toBeInTheDocument();
+    });
 
     //Make sure you are following semantic HTML and ARIA roles guidelines.
 
     expect.extend(toHaveNoViolations);
 
-    test('About screen should have no accessibility violations', async () => {
-        const {container} = render(
+    test("About screen should have no accessibility violations", async () => {
+        const { container } = render(
             <MemoryRouter>
                 <About />
-            </MemoryRouter>
+            </MemoryRouter>,
         );
         const results = await axe(container);
         expect(results).toHaveNoViolations();
     });
 
     //Make sure the purpose paragraph is present and contains the correct text.
-    test('renders the purpose paragraph', () => {
-    
+    test("renders the purpose paragraph", () => {
         render(
             <MemoryRouter>
                 <About />
-            </MemoryRouter>
+            </MemoryRouter>,
         );
 
-        const Paragraph = screen.getByTestId('purpose-paragraph');
+        const Paragraph = screen.getByTestId("purpose-paragraph");
         expect(Paragraph).toBeInTheDocument();
         expect(Paragraph).toHaveTextContent(/The purpose of SpecEffect is to/i);
     });
--- Code/spec_effect/src/Tests/Database.test.tsx
+++ Code/spec_effect/src/Tests/Database.test.tsx
@@ -1,36 +1,40 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getCountFromServer } from "firebase/firestore";
-import { projectId } from "./DatabaseConsts";
-import 'setimmediate'
+import {
+    getFirestore,
+    collection,
+    getCountFromServer,
+} from "firebase/firestore";
+import { projectId } from "../DatabaseConsts";
+import "setimmediate";
 
 // Replace with your actual Firebase config from the console
 const firebaseConfig = {
-  projectId: projectId
+    projectId: projectId,
 };
 
 describe("Check if database collections exist", () => {
     const app = initializeApp(firebaseConfig);
     const db = getFirestore(app);
     test("CPU collection exists", async () => {
-        const cpusCollection = collection(db, 'cpus');
+        const cpusCollection = collection(db, "cpus");
         const snapshot = await getCountFromServer(cpusCollection);
         const totalCpus = snapshot.data().count;
         expect(totalCpus).toBeGreaterThan(0);
     });
     test("GPU collection exists", async () => {
-        const gpusCollection = collection(db, 'gpus');
+        const gpusCollection = collection(db, "gpus");
         const snapshot = await getCountFromServer(gpusCollection);
         const totalGpus = snapshot.data().count;
         expect(totalGpus).toBeGreaterThan(0);
     });
     test("Laptop collection exists", async () => {
-        const laptopsCollection = collection(db, 'laptops');
+        const laptopsCollection = collection(db, "laptops");
         const snapshot = await getCountFromServer(laptopsCollection);
         const totalLaptops = snapshot.data().count;
         expect(totalLaptops).toBeGreaterThan(0);
     });
     test("Glossary collection exists", async () => {
-        const glossaryCollection = collection(db, 'glossary');
+        const glossaryCollection = collection(db, "glossary");
         const snapshot = await getCountFromServer(glossaryCollection);
         const totalGlossaryItems = snapshot.data().count;
         expect(totalGlossaryItems).toBeGreaterThan(0);
