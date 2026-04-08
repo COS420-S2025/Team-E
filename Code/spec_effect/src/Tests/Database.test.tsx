37591beac24b7bd64f0b19b097cf7da2f99b94f6
eliotlc <eliot.cole@maine.edu>
2026-04-08 18:26:53 -0400

    created firebase emulator and database tests

--- Code/spec_effect/src/Tests/Database.test.tsx
+++ Code/spec_effect/src/Tests/Database.test.tsx
@@ -3,18 +3,15 @@ import {
     getFirestore,
     collection,
     getCountFromServer,
+    addDoc,
+    FirestoreError,
+    deleteDoc
 } from "firebase/firestore";
-import { projectId } from "../DatabaseConsts";
+import { app, auth, db } from "../firebase-config";
 import "setimmediate";
-
-// Replace with your actual Firebase config from the console
-const firebaseConfig = {
-    projectId: projectId,
-};
+import { signInWithEmailAndPassword, signOut } from "firebase/auth";
 
 describe("Check if database collections exist", () => {
-    const app = initializeApp(firebaseConfig);
-    const db = getFirestore(app);
     test("CPU collection exists", async () => {
         const cpusCollection = collection(db, "cpus");
         const snapshot = await getCountFromServer(cpusCollection);
@@ -38,5 +35,107 @@ describe("Check if database collections exist", () => {
         const snapshot = await getCountFromServer(glossaryCollection);
         const totalGlossaryItems = snapshot.data().count;
         expect(totalGlossaryItems).toBeGreaterThan(0);
+    });
+});
+
+
+describe("Ensure non-admins can't write to database collections", () => {
+
+    test("CPU collection disallows writing for non-admins", async () => {
+        const collectionToTest = collection(db, "cpus");
+        const data = { "name": "TEST"};
+
+        // Added await here!
+        await expect(addDoc(collectionToTest, data)).rejects.toThrow(
+            expect.objectContaining({
+                code: 'permission-denied',
+            })
+        );
+    });
+    test("GPU collection disallows writing for non-admins", async () => {
+        const collectionToTest = collection(db, "gpus");
+        const data = { "name": "TEST"};
+
+        // Added await here!
+        await expect(addDoc(collectionToTest, data)).rejects.toThrow(
+            expect.objectContaining({
+                code: 'permission-denied',
+            })
+        );
+    });
+    test("Laptops collection disallows writing for non-admins", async () => {
+        const collectionToTest = collection(db, "laptops");
+        const data = { "name": "TEST"};
+
+        // Added await here!
+        await expect(addDoc(collectionToTest, data)).rejects.toThrow(
+            expect.objectContaining({
+                code: 'permission-denied',
+            })
+        );
+    });
+    test("Glossary collection disallows writing for non-admins", async () => {
+        const collectionToTest = collection(db, "glossary");
+        const data = { "term": "TEST", "definition": "test"};
+
+        // Added await here!
+        await expect(addDoc(collectionToTest, data)).rejects.toThrow(
+            expect.objectContaining({
+                code: 'permission-denied',
+            })
+        );
+    });
+});
+
+describe("Ensure admins can write to database collections", () => {
+
+    beforeAll(async () => {
+        await signInWithEmailAndPassword(auth, "admin@test.com", "password");
+    });
+
+    afterAll(async () => {
+        await signOut(auth);
+    });
+
+    test("CPU collection allows writing and deleting for admins", async () => {
+        const collectionToTest = collection(db, "cpus");
+        const data = { "name": "TEST"};
+
+        const docRef = await addDoc(collectionToTest, data);
+        
+        expect(docRef.id).toBeDefined();
+
+        await deleteDoc(docRef);
+    });
+
+    test("GPU collection allows writing for admins", async () => {
+        const collectionToTest = collection(db, "gpus");
+        const data = { "name": "TEST"};
+
+        const docRef = await addDoc(collectionToTest, data);
+        
+        expect(docRef.id).toBeDefined();
+
+        await deleteDoc(docRef);
+    });
+    test("Laptops collection allows writing for admins", async () => {
+        const collectionToTest = collection(db, "laptops");
+        const data = { "name": "TEST"};
+
+        const docRef = await addDoc(collectionToTest, data);
+        
+        expect(docRef.id).toBeDefined();
+
+        await deleteDoc(docRef);
+    });
+    test("Glossary collection allows writing for admins", async () => {
+        const collectionToTest = collection(db, "glossary");
+        const data = { "term": "TEST", "definition": "test"};
+
+        const docRef = await addDoc(collectionToTest, data);
+        
+        expect(docRef.id).toBeDefined();
+
+        await deleteDoc(docRef);
     });
 });
---
+++ Code/spec_effect/src/firebase-config.tsx
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAQ-7-zmf-vopfjZyFQPrAOw7y8Z2_p6hI",
+  authDomain: "speceffect-6f94d.firebaseapp.com",
+  projectId: "speceffect-6f94d",
+  storageBucket: "speceffect-6f94d.firebasestorage.app",
+  messagingSenderId: "405845550053",
+  appId: "1:405845550053:web:c19511f5f08a10a668050c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+const auth  = getAuth(app);
+
+if (window.location.hostname == "localhost")
+{
+  connectFirestoreEmulator(db, "localhost", 8080);
+  connectAuthEmulator(auth, "http://localhost:9099");
+}
+
+export {app, db, auth}
