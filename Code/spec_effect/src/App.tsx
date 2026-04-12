8ce517976effd113a08c3579081679fa5b701765
Aragon9723 <160268275+Aragon9723@users.noreply.github.com>
2026-04-12 14:26:08 -0400

    Moving login button

--- Code/spec_effect/src/App.tsx
+++ Code/spec_effect/src/App.tsx
@@ -52,6 +52,11 @@ function App() {
                 <button className="App-headerButtonStyle" onClick={goToAbout}>
                     About
                 </button>
+                <button
+                    className="App-headerButtonStyle"
+                    onClick={goToAdminLogin}>
+                    Admin Login
+                </button>
             </header>
             <Routes>
                 <Route path="/" element={<Home />} />
@@ -59,7 +64,6 @@ function App() {
                 <Route path="/glossary" element={<Glossary />} />
                 <Route path="/adminlogin" element={<AdminLogin />} />
             </Routes>
-            <button onClick={goToAdminLogin}>Go to admin login page</button>
         </div>
     );
 }
