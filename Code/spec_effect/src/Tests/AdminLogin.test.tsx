a4501cb3578431cc4943e98c4706970310cf674e
Aragon9723 <160268275+Aragon9723@users.noreply.github.com>
2026-04-15 22:49:28 -0400

    Added in success message and more tests

--- Code/spec_effect/src/Tests/AdminLogin.test.tsx
+++ Code/spec_effect/src/Tests/AdminLogin.test.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import AdminLogin from "../pages/AdminLogin";
+import userEvent from "@testing-library/user-event";
+
+const user = userEvent.setup();
 
 function renderLoginPage() {
     render(
@@ -34,5 +37,46 @@ describe("The page is visually loaded.", () => {
         renderLoginPage();
         const loginButton = screen.getByRole("button", { name: /Login/i });
         expect(loginButton).toBeInTheDocument();
+    });
+});
+
+describe("The page is functional.", () => {
+    test("Email input field accepts text", () => {
+        renderLoginPage();
+        const emailInp = screen.getByLabelText(/Email:/i) as HTMLInputElement;
+        emailInp.value = "test@example.com";
+        expect(emailInp.value).toBe("test@example.com");
+    });
+
+    test("Password input field accepts text", () => {
+        renderLoginPage();
+        const passInp = screen.getByLabelText(/Password:/i) as HTMLInputElement;
+        passInp.value = "password123";
+        expect(passInp.value).toBe("password123");
+    });
+
+    test("Login button is clickable", () => {
+        renderLoginPage();
+        const loginButton = screen.getByRole("button", { name: /Login/i });
+        expect(loginButton).toBeEnabled();
+    });
+
+    test("Displays error message on failed login", async () => {
+        renderLoginPage();
+        const emailInp = screen.getByLabelText(/Email:/i) as HTMLInputElement;
+        const passInp = screen.getByLabelText(/Password:/i) as HTMLInputElement;
+        const loginButton = screen.getByRole("button", { name: /Login/i });
+        emailInp.value = "test@example.com";
+        passInp.value = "wrongpassword";
+        await userEvent.click(loginButton);
+    });
+    test("Displays success message on successful login", async () => {
+        renderLoginPage();
+        const emailInp = screen.getByLabelText(/Email:/i) as HTMLInputElement;
+        const passInp = screen.getByLabelText(/Password:/i) as HTMLInputElement;
+        const loginButton = screen.getByRole("button", { name: /Login/i });
+        emailInp.value = "admin@test.com";
+        passInp.value = "password";
+        await userEvent.click(loginButton);
     });
 });
