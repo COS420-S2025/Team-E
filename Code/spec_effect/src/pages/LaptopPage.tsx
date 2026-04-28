0bac9ea4274073d816798c19c7e59e121d2a953c
GraceC-326 <grace.a.cochran@maine.edu>
2026-04-28 19:22:05 -0400

    Implemented list of specs in LaptopPage.tsx

--- Code/spec_effect/src/pages/LaptopPage.tsx
+++ Code/spec_effect/src/pages/LaptopPage.tsx
@@ -44,12 +44,17 @@ const LaptopPage = () => {
 
                             <div className="App-colBox">
                                 <h3 style={{margin: "2px", marginBottom: "17px"}}>Specs</h3>
-                                insert stuff from firebase here 
-                            </div>
-                            
-                            <div className="App-colBox">
-                                <h3 style={{margin: "2px", marginBottom: "17px"}}>Uses</h3>
-                                insert stuff from firebase here 
+                                <ul style={{listStyleType: "none", marginLeft: "4vw", textAlign: "left", lineHeight: "30px"}}>
+                                    <li> <b>CPU Type:</b> {laptop.cpuName}</li>
+                                    <li> <b>Cores:</b> {laptop.cpuCoreCount}</li>
+                                    <li> <b>Memory/RAM Size:</b> {laptop.memoryGb} GB</li>
+                                    <li> <b>GPU Type:</b> {laptop.gpuName || "N/A"}</li>
+                                    <li> <b>GPU VRAM Size:</b> {laptop.gpuVramMb} MB</li>
+                                    <li> <b>Disk Storage Type:</b> <span style={{ textTransform: 'uppercase' }}>{laptop.storageType}</span></li>
+                                    <li> <b>Storage Size:</b> {laptop.storageCapacityGb} GB</li>
+                                    <li> <b>Estimated Price:</b> ${laptop.priceCents/100}</li>
+                                </ul>
+
                             </div>
 
                         </div>  
